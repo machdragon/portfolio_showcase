10f42d7f5bd540debd1844fd57ddf45bd0917de7
machdragon <machdragon963852741@gmail.com>
2022-04-20 11:26:25 -0700

    update title

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -31,10 +31,12 @@ export default class MyDocument extends Document {
     return (
       <Html lang='en-GB'>
         <Head>
-          <link rel="icon" type="image/x-icon" href="static/favicon.ico" />
           <link rel="icon" type="image/x-icon" href="public/favicon.ico" />
           <link href="https://fonts.googleapis.com/css2?family=Space+Grotesk:wght@300;400;500;600;700&display=swap" rel="stylesheet"/>
         </Head>
+        <title>
+          Portfolio Showcase
+        </title>
         <body>
           <Main />
           <NextScript />
